9ecceec4a1bbac8d4e324f1353d5ca706698d7e0
Andrea Corsetti <andrea.corsetti94@gmail.com>
2019-09-27 14:22:54 +0200

    cleaning unused classes

--- src/main/java/com/acorsetti/core/service/probabilities/FixtureOutcomesEvaluatorService.java
+++
@@ -1,9 +0,0 @@
-package com.acorsetti.core.service.probabilities;
-
-import com.acorsetti.core.model.eval.FixtureEvals;
-import com.acorsetti.core.model.eval.FixtureOutcomes;
-
-public interface FixtureOutcomesEvaluatorService {
-
-    FixtureEvals evaluate(FixtureOutcomes fixtureOutcomes);
-}
